a47173bfeb6e5722dfdeee161f8f10cdc3fdb992
Artur Assis Alves <60040236+ArturAssisComp@users.noreply.github.com>
2022-09-28 07:49:10 -0300

    Server (#2)
    * Parallel Connection and simple server
    
    * Connect localhost, http 1.0 header, unfinished read directory
    
    * Change argument name from dir to base_dir
    
    * Update logging configurations
    
    * Implement file not found response
    
    Co-authored-by: lucas-maia-morais <lucasmaiamorais@gmail.com>

--- src/client/client.py
+++ src/client/client.py
@@ -1,3 +1,4 @@
+from threading import Thread
 import socket
 import logging
 import sys
@@ -33,5 +34,17 @@ class Client(object):
 
 
 if __name__=='__main__':
+    
+    # Example 1: Connect google
+    # my_client = Client()
+    # my_client.connect("www.google.com", 80, "GET /robots.txt HTTP/1.1\r\n\r\n", timeout=10)
+
+    # Example 2: Parallel connection to server
+    # my_client_1 = Client()
+    # my_client_2 = Client()
+    # Thread(target=my_client_1.connect, args=("127.0.1.1", 14000, "Temporario 1"), kwargs={'timeout': 10}).start()
+    # Thread(target=my_client_1.connect, args=("127.0.1.1", 14000, "Temporario 2"), kwargs={'timeout': 10}).start()
+
+    # Example 3: Connect localhost
     my_client = Client()
-    my_client.connect("www.google.com", 80, "GET /robots.txt HTTP/1.1\r\n\r\n", timeout=10)
+    my_client.connect("localhost", 14000, "GET /robots.txt HTTP/1.1\r\n\r\n", timeout=10)
